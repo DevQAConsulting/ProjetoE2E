24fe2f14dfe51b16896b5f5062c7d0aee8c0cb4e
Tiago <tiago.costa@millennium.com.br>
2025-07-07 20:23:18 -0300

    "Retira a execução incognita para abrir o browser localmente"

--- src/test/java/Runners/RunnerBase.java
+++ src/test/java/Runners/RunnerBase.java
@@ -31,7 +31,7 @@ public class RunnerBase {
         options.addArguments("user-data-dir=/tmp/profile-" + UUID.randomUUID());
 
         // Incognito + janela maximizada (opcional, útil para cenários visuais)
-        options.addArguments("incognito");
+       // options.addArguments("incognito");
         options.addArguments("start-maximized");
 
         driver = new ChromeDriver(options);
